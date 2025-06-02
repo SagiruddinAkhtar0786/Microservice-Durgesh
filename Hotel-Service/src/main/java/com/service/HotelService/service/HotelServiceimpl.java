b0f9dbe74d76746c3391c6d0d73fd687ad44fced
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-06-02 12:19:11 +0530

    commit

--- Hotel-Service/src/main/java/com/service/HotelService/service/HotelServiceimpl.java
+++ Hotel-Service/src/main/java/com/service/HotelService/service/HotelServiceimpl.java
@@ -1,6 +1,7 @@
 package com.service.HotelService.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,10 @@ public class HotelServiceimpl implements HotelService{
 	@Override
 	public Hotel create(Hotel hotel) {
 		// TODO Auto-generated method stub
+		
+		
+		String hotelId = UUID.randomUUID().toString();
+		hotel.setId(hotelId);
 		return hotelRepository.save(hotel);
 	}
 
