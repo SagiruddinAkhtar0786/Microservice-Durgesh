e37e215336bcf06c6c2ebef3427792d344231bbb
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-06-17 20:31:25 +0530

    commit

--- Microservice-Project/src/main/java/com/user/service/MicroServiceProject.java
+++ Microservice-Project/src/main/java/com/user/service/MicroServiceProject.java
@@ -2,8 +2,10 @@ package com.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroServiceProject {
 
 	public static void main(String[] args) {
