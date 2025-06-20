291acacbf6db1317e04a50e1e389bb236fe3203b
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-06-20 07:15:23 +0530

    commit

--- Microservice-Project/src/main/java/com/user/service/Service/Impl/UserServiceImplemetation.java
+++ Microservice-Project/src/main/java/com/user/service/Service/Impl/UserServiceImplemetation.java
@@ -1,20 +1,24 @@
 package com.user.service.Service.Impl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.user.service.Entity.Hotel;
 import com.user.service.Entity.Rating;
 import com.user.service.Entity.UserEntity;
 import com.user.service.Exception.ResourceNotFoundException;
 import com.user.service.Repository.UserRepository;
 import com.user.service.Service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Service
 public class UserServiceImplemetation implements UserService {
@@ -25,7 +29,7 @@ public class UserServiceImplemetation implements UserService {
 	@Autowired
 	private RestTemplate restTemplate;
 	
-//	private Logger logger = LoggerFactory.getLogger(UserServiceImplemetation.class);
+	private Logger logger = LoggerFactory.getLogger(UserServiceImplemetation.class);
 	
 
 	@Override
@@ -58,11 +62,26 @@ public class UserServiceImplemetation implements UserService {
 		// fetch rating of the above user from rating service
 		//		ArrayList<Rating> ratingsOfuser = restTemplate.getForObject("http://localhost:8083/ratings/users/0aa036a7-2aa4-4641-b4bd-b3763e82c1ed", ArrayList.class);
 
-		ArrayList<Rating> ratingsOfuser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
-		//logger.info("information ",forObject);
-		System.out.println("forObject >> "+ratingsOfuser);
+	Rating [] ratingsOfuser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), Rating[].class);
+		logger.info("information ",ratingsOfuser.toString());
+		System.out.println("forObject >> "+ratingsOfuser.toString());
+		System.out.println("sagir");
 		
-		user.setRatings(ratingsOfuser);
+		List<Rating> ratings = Arrays.stream(ratingsOfuser).toList();
+		
+		List<Rating> ratingList = ratings.stream().map(rating -> {
+			// api call to hotel service to get the hotel
+			//localhost:8082/hotels/2b3536db-d648-4988-9664-4b7aa6543df4
+			ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://localhost:8082/hotels/"+rating.getHotelId(), Hotel.class);
+			Hotel hotel = forEntity.getBody();
+			logger.info("responseStatus code {} ",forEntity.getStatusCode());
+			
+			//set the hotel to rating;
+			rating.setHotel(hotel);
+			
+			return rating;
+		}).collect(Collectors.toList());
+		user.setRatings(ratingList);
 		return user;
 	}
 
