4b88dda392a7a43a89cbacb9b01a85d3c6f5ada1
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-06-20 08:21:17 +0530

    commit

--- Microservice-Project/src/main/java/com/user/service/external/Services/RatingService.java
+++ Microservice-Project/src/main/java/com/user/service/external/Services/RatingService.java
@@ -1,7 +1,10 @@
 package com.user.service.external.Services;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.user.service.Entity.Rating;
 
@@ -12,8 +15,15 @@ public interface RatingService {
 	
 	//POST
 	@PostMapping("/ratings")
-	Rating createRating();
+	Rating createRating(Rating values);
 	
 	// PUT
+	
+	@PutMapping("/ratings/{ratingId}")
+	Rating updateRating(@PathVariable("ratingId") String ratingId, Rating rating);
+	
+	// Delete
+	@DeleteMapping("/ratings/{ratingId}")
+	void deleteRating(@PathVariable String ratingId);
 
 }
